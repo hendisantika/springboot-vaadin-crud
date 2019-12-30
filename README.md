5b446e6d6b152fde78dbb657c66e6ee313968145
Hendi Santika <hendi.santika@allianz.co.id>
2019-12-30 13:22:46 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-vaadin-crud
---
+++ src/test/java/com/hendisantika/springbootvaadincrud/SpringbootVaadinCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootvaadincrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVaadinCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
